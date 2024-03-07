9aa2a50ea8ab2bc02d02680e1bf733816d30a624
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2024-03-07 14:16:03 +0100

    Vcf (#5)
    * Initial commit
    
    * Update DESCRIPTION
    
    * bootstrap tests
    
    * Test CI
    
    * Prototype function to load gene expression data to SE
    
    * Stub functions
    
    * Add progress bar
    
    * Add pre-commit config
    
    * Add personalis functions to package and run roxygen
    
    * Add relevant documentation on personalis outputs
    
    * Update exploratory notebook
    
    * Fix pre-commit prettier
    
    * Format files according to pre-commit checks
    
    * Draft function to read small variant data
    
    * configure lintr
    
    * Implement basic functionality to read variant and GEX data into MAE
    
    * Implement basic error handling for missing samples
    
    * Generalize warning for missing samplese
    
    * Read CNV data
    
    * Fix CNV IO function in case of empty tables
    
    * Add function to read personalis HLA data
    
    * Add function to read in TCR data
    
    * Scrape TCR summary statistics from HTML
    
    * Implement function to read somatic variant statistics
    
    * Read summary stats for somatic variants
    
    * Implement bumpy_matrix_to_df
    
    * Read CNV summary statistics
    
    * Read MSI info
    
    * refactor
    
    * Workaround for samples with no col in bumpy matrix
    
    * Apply the fix also to small variant data
    
    * Use "Genomic Variant" instead of pos as unique variant identifier
    
    * Fix issue with reading non-somatic variants
    
    * Handle case when there are no samples for a modality
    
    * Fix duplicated mutation ids
    
    * Fix column name incompatibility in newer HTML report versions
    
    * stub vignette
    
    * Add vignette
    
    * Update vignette
    
    * Ensure bumpy matrix, row and coldata have consistent order
    
    * Fix alternative gex filename and CNV import
    
    * Support alternative TCR path
    
    * Fix column conversion in CNV reader
    
    * Fix paths
    
    * add function for parsing VCF files
    
    * add functionality for reading and storing VCF data
    
    * add/change comments
    
    * add option to read small variant reports of type all
    
    * Angewendeter Vorschlag
    
    * Angewendeter Vorschlag
    
    * add sample type check
    
    * Angewendeter Vorschlag
    
    * Angewendeter Vorschlag
    
    * add report_type parameter
    
    * Update README
    
    * Fix reading CNV report
    
    * Roxygenize
    
    * Fix parse copy number report
    
    ---------
    
    Co-authored-by: Christopher Mohr <christopher.mohr@boehringer-ingelheim.com>
    Co-authored-by: grst <grst@users.noreply.github.com>

--- R/util.R
+++ R/util.R
@@ -114,3 +114,31 @@ add_dummy_entry <- function(df, col_data, sample_col = "sample") {
     dummy_entries
   )
 }
+
+#' Parse VCF files for a provided path and construct data frame.
+#'
+#' @param path path to VCF file in `*.vcf` or `*.vcf.gz` format
+#' @return {tibble} new data frame with all variants (fixed field and genotype information)
+#' @importFrom dplyr mutate left_join
+#' @importFrom vcfR read.vcfR vcfR2tidy
+#' @importFrom stringr str_split_i
+parse_vcf_to_df <- function(path) {
+  # parse VCF file
+  vcf_content <- read.vcfR(path)
+  
+  # fixed field content to data frame
+  fixed_df <- vcfR2tidy(vcf_content)$fix
+  
+  # GT content to data frame
+  gt_df <- vcfR2tidy(vcf_content)$gt
+  
+  # create addition column with observed nucleotides in order to avoid collisions when we do the left_join
+  gt_df <- gt_df %>%
+    dplyr::mutate(ALT = str_split_i(gt_GT_alleles, "/", 2))
+  
+  # next use ChromKey, POS and ALT for joining vcf content data frames
+  joined_vcf_df <- fixed_df %>%
+    dplyr::left_join(gt_df, by = c("ChromKey", "POS", "ALT"))
+  
+  as_tibble(joined_vcf_df)
+}
